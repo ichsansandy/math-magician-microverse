70c4504ec1e284ccf1b0020a461eec2b7beaa0e6
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-07-05 16:11:36 +0700

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import Quotes from './components/Quotes';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <Quotes />
-      <Calculator />
+      <ul>
+        <NavLink
+          to="/"
+          style={(isActive) => ({
+            color: isActive ? 'green' : 'blue',
+          })}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/calculator"
+          style={(isActive) => ({
+            color: isActive ? 'green' : 'blue',
+          })}
+        >
+          Calculator
+        </NavLink>
+        <NavLink
+          to="/quote"
+          style={(isActive) => ({
+            color: isActive ? 'green' : 'blue',
+          })}
+        >
+          Quote
+        </NavLink>
+      </ul>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quotes />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/LandingPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to Math Magician</h1>
+      <p>
+        Welcome to our innovative calculator website, where crunching numbers has never been easier!
+        Whether you are a student, a professional, or simply someone in need of quick calculations,
+        we have you covered. Our user-friendly interface and extensive range of calculators
+        cater to various needs, from basic arithmetic to complex scientific equations.
+        With our cutting-edge technology and seamless design, you can effortlessly perform
+        mathematical operations, convert units, calculate percentages, and much more. Say goodbye
+        to manual calculations and let our calculators streamline your workflow. Start
+        maximizing your productivity today and unlock the power of precision with our intuitive
+        calculator website.
+      </p>
+    </div>
+  );
+}
+
+export default LandingPage;
